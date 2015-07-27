ede8e6fd74c72fd93b901d10220210ad2fd0bdee
Aaron Uthayagumaran <aaron.u6@gmail.com>
2015-07-27 14:29:15 -0400

    Fixed the project page

--- wp-content/themes/taglab-theme/single-projects.php
+++ wp-content/themes/taglab-theme/single-projects.php
@@ -145,12 +145,12 @@
 							 							  $selfie = wp_get_attachment_url(get_post_thumbnail_id($post->ID,'featured'));
 	    											?>
 													<div class="card-thumbnail">
-														<div class='card-thumbnail-img' style='background-image: url(<?php echo $selfie; ?>); '>
+														<div class='card-thumbnail-img' style='background-image: url(<?php echo $selfie; ?>); height: 15em;'>
 														</div>
 													</div>	
 												<?php endif; ?>
 												<!--The Title-->
-												<center><h2><?php the_title(); ?></h2></center>
+												<div><center><h2><?php the_title(); ?></h2></center></div>
 											</article>
 										</div>
 										</a>
@@ -160,56 +160,56 @@
 						</div>
 					</div>
 				</div>
-					
-				<!-- Other Projects -->
-				<div class="small-9 small-centered columns paragraph">
-					<div class='row'>
-						<h3>Other Projects</h3>
-						<div class="small-12 large-12 small-centered columns projectsbox" data-equalizer='projects'>
-							<!--Accessing the Posts from projects-->
-							<?php $args = array( 'post_type' => 'projects'); ?>
-							<?php $loop = new WP_Query( $args ); ?>
-							<?php $c = 0; ?>
-							<!--THE LOOP-->
-							<!--Purpose: To loop through all given posts of the given Post Type (projects)-->
-							<!--Condition: The loop will end when there are no more posts-->
-							<?php if ( $loop -> have_posts() ) : while ( $loop -> have_posts() && $c < 3) : $loop -> the_post(); ?>
-								<?php $T = get_the_title();?>
-								<?php if($title!=$T):?>
-									<!--Card Container-->
-									<div class="small-12 medium-6 large-4 columns end">
-										<div class="small-11 small-centered columns card" data-equalizer-watch='projects'>
-											<article class='post' data-equalizer-watch='projects'>
-												<!--THUMBNAIL-->
-												<!--Purpose: If there exists a thumbnail then display the thumbnail-->
-												<?php if (has_post_thumbnail()) : ?>
-													<?php $other = '';
-														  $other = wp_get_attachment_url(get_post_thumbnail_id($post->ID,'featured'));
-	    											?>
-													<div class="card-thumbnail">
-														<div class='card-thumbnail-img' style='background-image: url(<?php echo $other; ?>); height: 12em;'>
-														</div>
-													</div>	
-												<?php endif; ?>
-											
-												<!--CARD CONTENT-->
-												<!--Purpose: If there exists content display the content as an excerpt-->
-												<div class='card-content'>
-													<!--The Title-->
-													<h2> <?php the_title();?> </h2>
-													<!--CARD LINK-->
-													<!--Purpose: Give the link to the full article-->
-													<a class='card-link' href='<?php the_permalink();?>'>Explore</a>
-												</div>
-											</article>
-										</div>
+			</div><!-- End Large White Box -->
+			
+			<!-- Other Projects -->
+			<div class="small-9 small-centered columns paragraph">
+				<div class='row'>
+					<h3>Other Projects</h3>
+					<div class="small-12 large-12 small-centered columns projectsbox" data-equalizer='projects'>
+						<!--Accessing the Posts from projects-->
+						<?php $args = array( 'post_type' => 'projects'); ?>
+						<?php $loop = new WP_Query( $args ); ?>
+						<?php $c = 0; ?>
+						<!--THE LOOP-->
+						<!--Purpose: To loop through all given posts of the given Post Type (projects)-->
+						<!--Condition: The loop will end when there are no more posts-->
+						<?php if ( $loop -> have_posts() ) : while ( $loop -> have_posts() && $c < 3) : $loop -> the_post(); ?>
+							<?php $T = get_the_title();?>
+							<?php if($title!=$T):?>
+								<!--Card Container-->
+								<div class="small-12 medium-6 large-4 columns end">
+									<div class="small-11 small-centered columns card" data-equalizer-watch='projects'>
+										<article class='post' data-equalizer-watch='projects'>
+											<!--THUMBNAIL-->
+											<!--Purpose: If there exists a thumbnail then display the thumbnail-->
+											<?php if (has_post_thumbnail()) : ?>
+												<?php $other = '';
+													  $other = wp_get_attachment_url(get_post_thumbnail_id($post->ID,'featured'));
+	    										?>
+												<div class="card-thumbnail">
+													<div class='card-thumbnail-img' style='background-image: url(<?php echo $other; ?>); height: 12em;'>
+													</div>
+												</div>	
+											<?php endif; ?>
+										
+											<!--CARD CONTENT-->
+											<!--Purpose: If there exists content display the content as an excerpt-->
+											<div class='card-content'>
+												<!--The Title-->
+												<h2> <?php the_title();?> </h2>
+												<!--CARD LINK-->
+												<!--Purpose: Give the link to the full article-->
+												<a class='card-link' href='<?php the_permalink();?>'>Explore</a>
+											</div>
+										</article>
 									</div>
-								<?php endif; ?>
-							<?php endwhile; endif;?>
-						</div>
+								</div>
+							<?php endif; ?>
+						<?php endwhile; endif;?>
 					</div>
 				</div>
-			</div><!-- End Large White Box -->
+			</div>
 		</div><!-- End Grey Box -->
 	</section>
 </div>
